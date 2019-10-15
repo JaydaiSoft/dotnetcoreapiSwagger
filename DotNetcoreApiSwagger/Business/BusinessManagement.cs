1e1a453872bbfcf645d28b4a32605b8c69a3c6b5
Akarapol Pranee <Akarappr@scg.com>
2019-10-15 18:28:49 +0700

    Add Business class Add CalculateNumberSeries

---
+++ DotNetcoreApiSwagger/Business/BusinessManagement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetcoreApiSwagger.Business
+{
+    public class BusinessManagement : IBusinessManagement
+    {
+        public BusinessManagement()
+        {
+
+        }
+
+        public string CalculateNumberSeries()
+        {
+            int[] numberSeries = new int[7];
+            numberSeries = RegonitionPattern(ref numberSeries);
+            return string.Format("X, 5, 9, 15, 23, Y, Z Result : X = {0}, Y = {1}, Z = {2}"
+                ,numberSeries[0], numberSeries[5], numberSeries[6]);
+        }
+
+        private int[] RegonitionPattern(ref int[] numberSeries)
+        {
+            for (int i = 0; i <= numberSeries.Length - 1; i++)
+            {
+                if (i == 0)
+                {
+                    numberSeries[0] = 3;
+                }
+                else
+                {
+                    int n = i + 1;
+                    numberSeries[i] = numberSeries[i - 1] + 2 * (n - 1);
+                }
+            }
+            return numberSeries;
+        }
+    }
+}
--- DotNetcoreApiSwagger/Controllers/SCGController.cs
+++ DotNetcoreApiSwagger/Controllers/SCGController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetcoreApiSwagger.Business;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,13 +12,18 @@ namespace DotNetcoreApiSwagger.Controllers
     [ApiController]
     public class SCGController : ControllerBase
     {
+        private IBusinessManagement _businessManagement;
+        public SCGController()
+        {
+            _businessManagement = new BusinessManagement();
+        }
+
         // GET api/values
         /// <summary>
         /// Get API Value
         /// </summary>
         /// <remarks>This API will get the values.</remarks>
-        [HttpGet]
-        [Route("TestGetValue")]
+        [HttpGet, Route("TestGetValue")]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
@@ -28,11 +34,32 @@ namespace DotNetcoreApiSwagger.Controllers
         /// Get API Alive
         /// </summary>
         /// <remarks>This API will get the values.</remarks>
-        [HttpGet]
-        [Route("IsAlive")]
+        [HttpGet, Route("IsAlive")]
         public ActionResult<bool> IsAlive()
         {
             return true;
+        }
+
+        // GET api alive
+        /// <summary>
+        /// X, 5, 9, 15, 23, Y, Z
+        /// </summary>
+        /// <remarks> function for finding X, Y, Z value </remarks>
+        [HttpGet, Route("GetNumberSeries")]
+        public ActionResult<string> GetNumberSeries()
+        {
+            string result = "";
+            try
+            {
+                result = _businessManagement.CalculateNumberSeries();
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                result = ex.GetBaseException().Message;
+                return result;
+            }
+            
         }
     }
 }
